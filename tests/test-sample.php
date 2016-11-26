db38f06d43e1df386846459d8244cbbe18429a24
Paul Bearne <pbearne@gmail.com>
2016-11-25 19:53:09 -0500

    removed unit test

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -4,7 +4,7 @@ class SampleTest extends WP_UnitTestCase {
 
 	function testSample() {
 		// replace this with some actual testing code
-		$this->assertTrue( true );
+	//	$this->assertTrue( true );
 	}
 }
 
